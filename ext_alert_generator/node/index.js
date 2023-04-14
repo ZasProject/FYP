01b8b44982a988541c61f424cc5b2d4d31a96077
Zeyad <zaswork99@gmail.com>
2023-04-14 18:52:05 +0100

    minor change to ext alert handler

--- ext_alert_generator/node/index.js
+++ ext_alert_generator/node/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const kafka = require('kafka-node');
 var nodemailer = require('nodemailer');
+const cors = require('cors');
 
 // allow cors for the client (UI) domain (here running locally on localhost:4200) to prevent api failure due to cross origin issue
 app.use(cors({
@@ -10,6 +11,7 @@ app.use(cors({
   methods: 'GET,POST'
 }));
 
+const Consumer = kafka.Consumer;
 //Create a new kafka client to listen on the 'close-alert' topic
 const alertGeneratorClient = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
 
@@ -29,9 +31,12 @@ externalAlertGenerator.on('message', function (message) {
     <p>Alert Details:</p>
     <p>${alertDetails.id} - ${alertDetails.type} - ${alertDetails.breachType} - ${alertDetails.reason}</p>
     <p> Alert closed with reasons - ${alertDetails.actionTaken}</p>
+    <p> Office Comments - ${alertDetails.comments}</p>
+
   </body>
   </html>`
 
+  console.log(formatMessage)
   // use nodemailer api to send data to email. Configure self email details - username password and the mailing service
   var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -47,15 +52,15 @@ externalAlertGenerator.on('message', function (message) {
     html: formatMessage
   };
   transporter.sendMail(mailOptions, function(error, info){
-    if(error) {
-      res.send(error);
-    } else {
-      res.send('successfully sent')
-    }
+    // if(error) {
+    //   res.send(error);
+    // } else {
+    //   res.send('successfully sent')
+    // }
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
 });
 
