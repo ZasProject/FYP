dd3d6f267ad51543cac31ce63d9ee26f720cd029
Zeyad Syed <zaswork99@gmail.com>
2023-04-23 16:08:10 +0100

    corrected json path and load param

--- data_feed/data_feed_controller/case1/load_source_data.sh
+++ data_feed/data_feed_controller/case1/load_source_data.sh
@@ -1,1 +1,1 @@
-/home/zeyad/kafka/confluent-7.3.3/bin/kafka-console-producer --bootstrap-server localhost:9092 --topic referenceTimeTopic --property parse.key=true --property key.separator=? < /home/zeyad/ams/prototype/datagenerator/source_data_case1.json
+/home/zeyad/kafka/confluent-7.3.3/bin/kafka-console-producer --bootstrap-server localhost:9092 --topic sourceDataTopic < /home/zeyad/ams/data_feed/data_generator/case1/source_data_case1.json
