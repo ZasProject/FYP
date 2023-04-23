358fdae26119a93ef32a1b17cb7e4a7d5f157524
Zeyad Syed <zaswork99@gmail.com>
2023-04-23 15:43:51 +0100

    added delete_kafka_topic.sh script

---
+++ scripts/kafka/delete_kafka_topic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export CONFLUENT_HOME=/home/zeyad/kafka/confluent-7.3.3
+export PATH=$CONFLUENT_HOME/bin:$PATH
+echo "*** Deleting Kafka Topic  ***"
+${CONFLUENT_HOME}/bin/kafka-topics -bootstrap-server localhost:9092 --delete --topic $1
