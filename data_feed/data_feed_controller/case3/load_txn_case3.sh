9b318f56050f07d4f5f150a06cf0daeb4a8312ba
Zeyad Syed <zaswork99@gmail.com>
2023-04-23 22:57:03 +0100

    corrected json path

--- data_feed/data_feed_controller/case3/load_txn_case3.sh
+++ data_feed/data_feed_controller/case3/load_txn_case3.sh
@@ -1,1 +1,1 @@
-/home/zeyad/kafka/confluent-7.3.2/bin/kafka-console-producer --bootstrap-server localhost:9092 --topic cardTransactionDataTopic < /home/zeyad/ams/data_feed/data_generator/case3/txn_case3.json
+/home/zeyad/kafka/confluent-7.3.3/bin/kafka-console-producer --bootstrap-server localhost:9092 --topic cardTransactionDataTopic < /home/zeyad/ams/data_feed/data_generator/case3/txn_case3.json
